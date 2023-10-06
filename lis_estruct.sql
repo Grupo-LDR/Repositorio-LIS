0943f41d40c36e69763f26b3174ff7306ac86dcc
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2023-10-06 09:19:49 -0300

    sql_final

--- lis_estruct.sql
+++ lis_estruct.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `address` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `date_birth_at` date DEFAULT NULL,
   `password` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -79,7 +79,6 @@ CREATE TABLE IF NOT EXISTS `access_permissions` (
  PRIMARY KEY(`id`,`id_employee`, `resource`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
 DROP TABLE IF EXISTS `audits`;
 CREATE TABLE IF NOT EXISTS `audits` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -87,8 +86,99 @@ CREATE TABLE IF NOT EXISTS `audits` (
   `id_studie` int UNSIGNED NOT NULL,
   `action` ENUM('create', 'update', 'delete')  NOT NULL DEFAULT 'create',
   `result`varchar (500) NOT NULL,
-  `comment`varchar (250) NOT NULL,
+  `comment`varchar (250) ,
   `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar roles';
+
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_user` int UNSIGNED NOT NULL,
+  `id_employee` tinyint UNSIGNED NOT NULL,
+  `doctor` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `diagnostico` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'estados de ordenes TABLA',
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment`varchar (250) ,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar ordenes estudio';
+
+DROP TABLE IF EXISTS `studies`;
+CREATE TABLE IF NOT EXISTS `studies` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_order` int UNSIGNED NOT NULL,
+  `id_employee` tinyint UNSIGNED NOT NULL COMMENT ' id del empelado que relaiza el estudio', 
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'estados de estudio TABLA',
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_validate` date DEFAULT NULL,
+  `comment`varchar (250) ,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar estudios a relaizar';
+
+DROP TABLE IF EXISTS `studie_results`;
+CREATE TABLE IF NOT EXISTS `studie_results` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_studie` int UNSIGNED NOT NULL,
+  `status` tinyint(1) DEFAULT '2' COMMENT 'estados de resultado en 1 valido, 0 no valido 2 en proceso ',
+  `values` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `values_standard` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL,
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_validate` date DEFAULT NULL,
+  `comment`varchar (250),
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar estudios a relaizar';
+
+DROP TABLE IF EXISTS `samples`;
+CREATE TABLE IF NOT EXISTS `samples` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_studie` int UNSIGNED NOT NULL,
+  `status` tinyint(1) DEFAULT '1' COMMENT 'estados de muestra 1 valido, 0 no valido',
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar muestras de pacientes';
+DROP TABLE IF EXISTS `tests`;
+CREATE TABLE IF NOT EXISTS `tests` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_test_type` int UNSIGNED NOT NULL,
+  `name` varchar(120) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `common_name` varchar(80) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0: inactivo 1 activo',
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment`varchar (250) ,
+  `time_process` int NOT NULL COMMENT 'en dias habiles',
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar esudios disponibles';
+
+DROP TABLE IF EXISTS `test_types`;
+CREATE TABLE IF NOT EXISTS `test_types` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment`varchar (250) ,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0: inactivo 1 activo',
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar esudios disponibles';
+
+CREATE TABLE IF NOT EXISTS `test_requirements` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_test` int UNSIGNED NOT NULL,
+  `requeriments`varchar (250) NOT NULL,
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment`varchar (250) ,
+  PRIMARY KEY (`id`)
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar los requsitos par acda estudio';
+
+DROP TABLE IF EXISTS `tests_standard_values`;
+CREATE TABLE IF NOT EXISTS `tests_standard_values` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id_test` int UNSIGNED NOT NULL,
+  `name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `value` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `date_create_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `comment`varchar (250) ,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0: inactivo 1 activo',
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='Tabla para registrar valores standard de estudios';
+
 COMMIT;
