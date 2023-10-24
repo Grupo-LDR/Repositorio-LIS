07ec09b18920ad68b757e97913999dee8e459517
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2023-10-23 22:18:55 -0300

    gitignoe

--- src/config.js
+++ src/config.js
@@ -7,19 +7,6 @@ for (const key in configEnv) {
     config[key] = configEnv[key];
 }
 console.log(config);
-
-
-// const config = {
-//     APP_PORT: process.env.APP_PORT,
-//     DB_HOST: process.env.DB_HOST,
-//     DB_PORT: process.env.DB_PORT,
-//     DB_USER: process.env.DB_USER,
-//     DB_PASS: process.env.DB_PASS,
-//     DB_DIALECT: process.env.DB_DIALECT,
-//     DB_DATABASE: process.env.DB_DATABASE,
-//     APP_NAME: process.env.APP_NAME
-
-// };
 export default config;
 
 
