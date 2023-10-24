4c88b9572416771f03a599699138c5d8c4562bb8
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2023-10-23 22:17:18 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
-const configEnv = dotenv.parse(fs.readFileSync('.env'));
+const configEnv = dotenv.parse(fs.readFileSync('.env', 'utf-8'));
 const config = {};
 for (const key in configEnv) {
     config[key] = configEnv[key];
