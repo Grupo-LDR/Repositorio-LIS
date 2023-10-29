88815d09e9fc3314da45f6ba9c610718e6284746
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2023-10-29 08:40:09 -0300

    ordenando coidgo

--- src/app.js
+++ src/app.js
@@ -1,28 +1,41 @@
-//import http_errors from 'http-errors';
+/**
+ * configuracion
+ * @ true: development
+ * @false: dproduction
+ *   
+ */
+const __DEV = false;
+
+/**
+ * modulo configuracion
+ */
+import Config from './config.js';
+const config = new Config(__DEV);
+/**
+ * modulos http
+ */
 import express from 'express';
 import compression from 'compression';
-//import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
-import config from './config.js';
-//import express_session from 'express-session';
 import morgan from 'morgan';
 import { fileURLToPath } from 'url';
+/**
+ * modulos path
+ */
 import { dirname } from 'path';
 import path from 'path';
-/** modulos routes  */
-// autenticacion
-//import AuthServer from './middlewares/authServer.js';
-// rutas  
-import LoginRouter from './routes/loginRouter.js';
-import UserRouter from './routes/userRouter.js';
-import ExamRouter from './routes/examRouter.js';
-import CityRouter from './routes/cityRouter.js';
-import orderRouter from './routes/orderRouter.js';
-// import IndexRouter from './routes/indexRouter.js';
-// import MainRouter from './routes/mainRouter.js';
-// import PatientRouter from './routes/patientRouter.js';
 
-// variables y constantes App
+/***
+ * modulos routes
+ */
+// import LoginRouter from './routes/loginRouter.js';
+// import UserRouter from './routes/userRouter.js';
+// import ExamRouter from './routes/examRouter.js';
+// import CityRouter from './routes/cityRouter.js';
+// import orderRouter from './routes/orderRouter.js';
+/**
+ * variables y constantes App
+ */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 console.clear();
@@ -31,6 +44,9 @@ class App {
         console.log("APP instanciada");
         this.app = express();
     }
+    /**
+    * Configuracion del servidor
+    */
     configureServer() {
         console.log("Configurando Server");
         this.app.use(express.json());
@@ -40,64 +56,57 @@ class App {
         console.log(__dirname);
         this.app.set('views', path.join(__dirname, 'views'));
         this.app.use(cookieParser());
-        this.loginRouter = new LoginRouter();
-        this.userRouter = new UserRouter();
-        this.examRouter = new ExamRouter();
-        this.cityRouter = new CityRouter();
-        this.orderRouter = new orderRouter();
-
-        // this.indexRouter = new IndexRouter();
-        // this.mainRouter = new MainRouter();
-        // this.patientRouter = new PatientRouter();
-        // this.authServer = new AuthServer();
-
-
+        /**
+         * instancias de Ruteo
+         */
+        //     this.loginRouter = new LoginRouter();
+        //     this.userRouter = new UserRouter();
+        //     this.examRouter = new ExamRouter();
+        //     this.cityRouter = new CityRouter();
+        //     this.orderRouter = new orderRouter();
     }
     /**
      * Ruteo de peticiones  
      */
     appServerRoute() {
         this.app.use(morgan('dev'));
-        // ruta archvios estaticos
+        /**
+         * Ruteo de peticiones estaticas
+         */
         this.app.use(express.static('./src/public'));
-        this.app.get('/', (req, res) => {
-            res.render('index.pug', { title: config.APP_TITLE });
-        });
-        // this.app.use('/', this.loginRouter.getRouter());
-        this.app.use('/user', this.userRouter.getRouter());
-        // ruteo Examenes 
-        this.app.use('/exam', this.examRouter.getRouter());
-        // ruteo Examenes 
-        this.app.use('/city', this.cityRouter.getRouter());
-        // ruteo de ordenes de trabajo
-        this.app.use('/order', this.orderRouter.getRouter());
-
-
-
-        //this.app.use('/user', this.userRouter.getRouter());
-        //this.app.use('/users', this.userRouter.getRouter());
-        // // rutas que no requieren auth
-        // console.log("Cargando manejador de rutas");
-        //this.app.use('/', this.indexRouter.getRouter());
-        //        this.app.use('/order', this.orderRouter.getRouter());
-        // login
-        // this.app.use('/main', this.mainRouter.getRouter());
-        // this.app.use('/main/patient', this.patientRouter.getRouter());
-
-        ///neworder/1
-        // // midlware de auth
-        // this.app.use(this.authServer.authUser);
-
-        // // rutas que requieren auth
+        /**
+         * Ruteo de peticiones  default index
+         */
+        // this.app.get('/', (req, res) => {
+        //     res.render('index.pug', { title: config.APP_TITLE });
+        // });
         // this.app.use('/user', this.userRouter.getRouter());
-        // // this.app.use('/users', this.userRouter.getRouter());
-        // //this.app.use('/studie', this.studieRouter.getRouter());
+        /**
+         * Ruteo Examenes
+         */
+        // this.app.use('/exam', this.examRouter.getRouter());
+        /** 
+         * Ruteo de ciudades
+        */
+        // this.app.use('/city', this.cityRouter.getRouter());
+        /**
+         * Ruteo de ordenes
+         */
+        // this.app.use('/order', this.orderRouter.getRouter());
+        /**
+         * Ruteo de autenticacion
+         */
+        //        this.app.use(this.authServer.authUser);
+        /**
+         * Ruteo de error
+         */
         this.app.use((req, res) => {
             res.status(404).send('Error 404 -< Página no encontrada');
         });
     }
-
-    // Inicio asincrónico
+    /**
+     * Inicio asincrónico
+     */
     async start() {
         try {
             console.log(`Iniciando APP en el puerto ${config.APP_PORT}`);
@@ -111,5 +120,4 @@ class App {
         }
     }
 }
-
 export default App;
--- src/config.js
+++ src/config.js
@@ -1,12 +1,26 @@
 import fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
-const configEnv = dotenv.parse(fs.readFileSync('.env', 'utf-8'));
-const config = {};
-for (const key in configEnv) {
-    config[key] = configEnv[key];
+
+class Config {
+    constructor(entorno) {
+        this.config = {};
+        this.configEnv = {};
+
+        if (entorno) {
+            this.configEnv = dotenv.parse(fs.readFileSync('.dev.env', 'utf-8'));
+        } else {
+            this.configEnv = dotenv.parse(fs.readFileSync('.prod.env', 'utf-8'));
+        }
+
+        console.log(this.configEnv);
+
+        for (const key in this.configEnv) {
+            this.config[key] = this.configEnv[key];
+        }
+
+        console.log(this.config);
+        return this.config;
+    }
 }
-console.log(config);
-export default config;
-
-
+export default Config;
