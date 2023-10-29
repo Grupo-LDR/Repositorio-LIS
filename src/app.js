6f8fd1f202a6c759cdbeb46bae123c2c55eb8334
Rafael Lopez <50971821+raffarraffa@users.noreply.github.com>
2023-10-29 12:30:42 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -9,14 +9,16 @@ const __DEV = false;
 /**
  * modulo configuracion
  */
-import Config from './config.js';
-const config = new Config(__DEV);
+import config from './config.js';
+// import Config from './config.js';
+// const config = new Config(__DEV);
 /**
  * modulos http
  */
 import express from 'express';
 import compression from 'compression';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import morgan from 'morgan';
 import { fileURLToPath } from 'url';
 /**
@@ -29,7 +31,7 @@ import path from 'path';
  * modulos routes
  */
 // import LoginRouter from './routes/loginRouter.js';
-// import UserRouter from './routes/userRouter.js';
+import UserRouter from './routes/userRouter.js';
 // import ExamRouter from './routes/examRouter.js';
 // import CityRouter from './routes/cityRouter.js';
 // import orderRouter from './routes/orderRouter.js';
@@ -38,7 +40,7 @@ import path from 'path';
  */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-console.clear();
+//console.clear();
 class App {
     constructor() {
         console.log("APP instanciada");
@@ -47,7 +49,15 @@ class App {
     /**
     * Configuracion del servidor
     */
+
+
     configureServer() {
+        const corsOptions = {
+            origin: '*', // Origen permitido
+            methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Métodos permitidos
+            allowedHeaders: 'Content-Type,Authorization', // Encabezados permitidos
+            optionsSuccessStatus: 204, // Código de estado para respuestas pre-vuelo OPTIONS
+        };
         console.log("Configurando Server");
         this.app.use(express.json());
         this.app.use(express.urlencoded({ extended: true }));
@@ -56,11 +66,12 @@ class App {
         console.log(__dirname);
         this.app.set('views', path.join(__dirname, 'views'));
         this.app.use(cookieParser());
+        this.app.use(cors(corsOptions));
         /**
          * instancias de Ruteo
          */
         //     this.loginRouter = new LoginRouter();
-        //     this.userRouter = new UserRouter();
+        this.userRouter = new UserRouter();
         //     this.examRouter = new ExamRouter();
         //     this.cityRouter = new CityRouter();
         //     this.orderRouter = new orderRouter();
@@ -77,10 +88,13 @@ class App {
         /**
          * Ruteo de peticiones  default index
          */
-        // this.app.get('/', (req, res) => {
-        //     res.render('index.pug', { title: config.APP_TITLE });
-        // });
-        // this.app.use('/user', this.userRouter.getRouter());
+        this.app.get('/', (req, res) => {
+            res.render('index.pug', { title: config.APP_TITLE });
+        });
+        /**
+         * Ruteo de peticiones user
+         */
+        this.app.use('/user', this.userRouter.getRouter());
         /**
          * Ruteo Examenes
          */
--- src/config.js
+++ src/config.js
@@ -2,25 +2,12 @@ import fs from 'fs';
 import dotenv from 'dotenv';
 dotenv.config();
 
-class Config {
-    constructor(entorno) {
-        this.config = {};
-        this.configEnv = {};
-
-        if (entorno) {
-            this.configEnv = dotenv.parse(fs.readFileSync('.dev.env', 'utf-8'));
-        } else {
-            this.configEnv = dotenv.parse(fs.readFileSync('.prod.env', 'utf-8'));
-        }
-
-        console.log(this.configEnv);
-
-        for (const key in this.configEnv) {
-            this.config[key] = this.configEnv[key];
-        }
-
-        console.log(this.config);
-        return this.config;
-    }
+const configEnv = dotenv.parse(fs.readFileSync('.dev.env', 'utf-8'));
+const config = {};
+for (const key in configEnv) {
+    config[key] = configEnv[key];
 }
-export default Config;
+console.log(config);
+export default config;
+
+
